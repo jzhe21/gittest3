c3e9d0284ecf490fd24c8d30f662789fe0094dc3
jzhe21 <jzhe21@hotmail.com>
2015-04-20 21:01:34 +0800

    secord commit

--- gittest/src/org/gittest/Test.java
+++ gittest/src/org/gittest/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git World");
+		System.out.println("Hello Git World1");
+		System.out.println("Hello Git World2");
 	}
 
 }
